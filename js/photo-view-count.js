5303b1d704ea5ed150bc914acc33642f0ca2e3ad
Syed Ehsan Ul Haq <softwaresmasters70@gmail.com>
2022-09-17 17:25:00 +0400

    update portfolio
    Portraits & Logo Animation

--- js/photo-view-count.js
+++ js/photo-view-count.js
@@ -1,24 +1,33 @@
-var photos=0;
-var views=0;
+var photos = 0;
+var views = 0;
 
-var contributeID = "103140580685995586585";//SAH new id
-var mapLink="https://www.google.com/maps/contrib/";
-//mircom 
+var contributeID = "103140580685995586585"; //SAH new id
+var mapLink = "https://www.google.com/maps/contrib/";
+//mircom
 // 112229672882478638212
 
-var photo_view = photo_view_match("(\\d+),\\\\\"(C[\\w-]{34,40})\\\\\",(\\d+)", getResponseText(mapLink + contributeID));
+var photo_view = photo_view_match(
+  '(\\d+),\\\\"(C[\\w-]{34,40})\\\\",(\\d+)',
+  getResponseText(mapLink + contributeID)
+);
 photos += parseInt(photo_view.photos);
 views += parseInt(photo_view.views);
 // console.log(photo_view);
 
 contributeID = "103474235754339879446";
-photo_view = photo_view_match("(\\d+),\\\\\"(C[\\w-]{34,40})\\\\\",(\\d+)", getResponseText(mapLink + contributeID));
+photo_view = photo_view_match(
+  '(\\d+),\\\\"(C[\\w-]{34,40})\\\\",(\\d+)',
+  getResponseText(mapLink + contributeID)
+);
 
 photos += parseInt(photo_view.photos);
 views += parseInt(photo_view.views);
 
 contributeID = "112229672882478638212";
-photo_view = photo_view_match("(\\d+),\\\\\"(C[\\w-]{34,40})\\\\\",(\\d+)", getResponseText(mapLink + contributeID));
+photo_view = photo_view_match(
+  '(\\d+),\\\\"(C[\\w-]{34,40})\\\\",(\\d+)',
+  getResponseText(mapLink + contributeID)
+);
 
 photos += parseInt(photo_view.photos);
 views += parseInt(photo_view.views);
@@ -38,10 +47,10 @@ function getResponseText(link) {
   var response;
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-          response = xhttp.responseText;
-      }
-  }
+    if (this.readyState == 4 && this.status == 200) {
+      response = xhttp.responseText;
+    }
+  };
   xhttp.open("GET", cors + link, false);
   xhttp.send();
   return response;
@@ -52,12 +61,11 @@ function photo_view_match(pattern, contents) {
   //console.log(matches[0].length);
   var arr = {};
   if (matches !== null && matches.length > 0) {
-      matches.forEach(function (arrayItem) {
-          arr.photos = arrayItem[1];
-          arr.views = arrayItem[3];
-          // console.log(arrayItem[0] + " - "+ arrayItem[1] + " - " + arrayItem[2]+" - "+arrayItem[3] );
-      });
+    matches.forEach(function (arrayItem) {
+      arr.photos = arrayItem[1];
+      arr.views = arrayItem[3];
+      // console.log(arrayItem[0] + " - "+ arrayItem[1] + " - " + arrayItem[2]+" - "+arrayItem[3] );
+    });
   }
   return arr;
 }
-
